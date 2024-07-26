f3db8ba526cf3997f8014ec0c5df3a9160756b54
romoch82 <romoch37@gmail.com>
2024-07-26 22:44:07 +0300

    homework2.py

--- homework2.py
+++ homework2.py
@@ -1,2 +1,8 @@
-name = "Domzadanie"
-print(name)
+name = "Pyton"
+print (name)
+print("всего_задач")
+print (12)
+print ("затрачено_часов")
+print (1.5)
+print("среднее_время_выполнения")
+print (1.5 / 12)
